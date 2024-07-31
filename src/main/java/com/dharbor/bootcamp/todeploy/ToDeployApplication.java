d17fa7216ce6674a24897b6c122e431ce14b7d80
Marco Serrano <marcoserrano008@gmail.com>
2024-07-30 20:38:18 -0400

    First commit

---
+++ src/main/java/com/dharbor/bootcamp/todeploy/ToDeployApplication.java
@@ -0,0 +1,13 @@
+package com.dharbor.bootcamp.todeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDeployApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDeployApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dharbor/bootcamp/todeploy/ToDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dharbor.bootcamp.todeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
